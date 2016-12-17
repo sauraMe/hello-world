87414f023ab52643a073ede6d8c1c15697a8e217
pepesaura <sauraMe@users.noreply.github.com>
2016-12-18 00:06:30 +0100

    Guiones (#2)
    * Javascript para Photoshop 
    
    Encaja en 1024px
    
    * Cambio en la definición
    
    * Muestra el nombre oficial de la fuente en uso

---
+++ LeerFont.jsx
@@ -0,0 +1,16 @@
+﻿/*
+	Archivo LeerFont.jsx, extraído de la guía 'PS 7.0 Scripting Guide.pdf' (pág. 66)
+	Cuando se trabaja con fonts en los scripts de PhotoShop hay que utilizar el nombre PostScript de la fuente
+	en lugar del nombre que presenta el selector de fuentes.
+
+	Para determinar el nombre PostScript hay que proceder como sigue:
+	a) Se crea una capa de texto utilizando el tipo de fuente cuyo nombre queremos determinar
+	b) Se ejecuta el script LeerFont y aparecerá un mensaje de alerta con el nombre
+	c) Utilizarlo tal cual.
+*/
+
+var capa = activeDocument.artLayers[0];
+if (capa.kind == LayerKind.TEXT) {
+		alert( "Nombre PostScript de la fuente actual:\r\n\r\n" + capa.textItem.font);
+		} else { alert( "Ummm. Parece que no has leido el script....\r\nEs necesario ejecutarlo sobre una capa de texto!" ); }
+capa = null;
